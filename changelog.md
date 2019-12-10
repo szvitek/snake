ef1f306511448a3e12bd30d1f7b1a986313e00a6
Gyula Szvitek <gy.szvitek@gmail.com>
2019-12-10 01:03:29 +0000

    feat(0.2): collision detection, score, better controls

---
+++ changelog.md
@@ -0,0 +1,14 @@
+# Changelog
+
+## 0.2
+
+- counting score (currently hidden)
+- collision detection added
+  - wall
+  - tail
+- improved controls:
+  - prevent to go to the opposite direction
+
+## 0.1
+
+- first work in progress "release"
--- snake.js
+++ snake.js
@@ -1,8 +1,12 @@
-// todo: maybe refactor to a snake class
-// todo: landing page, with start button
-// end screen with win/loose, maybe show score (eaten apples)
-// version: 0.1
+// todo: refactor to a snake class 🐍
+// todo: landing page, with start button 🔘
+// todo: end screen with win/loose, 🏆
+// todo: show score (eaten apples) 🍎
+// todo:  use vue.js to make life easier managing landing page/game/options/score/end screens
+// todo: turn app into pwa for offline play 📱
+// version: 0.2
 
+let score = 0;
 const size = 300;
 const scale = 10;
 let speed = 350;
@@ -31,21 +35,49 @@ const snake = [
   [10, 14]
 ];
 
-function update() {
+async function update() {
   context.clearRect(0, 0, 30, 30);
   const head = snake[0];
 
-  snake.unshift([head[0] + direction[0], head[1] + direction[1]]);
+  const newHeadX = head[0] + direction[0];
+  const newHeadY = head[1] + direction[1];
 
-  // todo: collisions: check [] wall, [] tail
+  // wall collide logic
+  let didCollideWall = false;
+  if (
+    newHeadX < 0 ||
+    newHeadY < 0 ||
+    newHeadX === size / scale ||
+    newHeadY === size / scale
+  ) {
+    didCollideWall = true;
+  }
+
+  snake.unshift([newHeadX, newHeadY]);
+
   // eat the apple
   if (snake[0][0] === apple[0] && snake[0][1] === apple[1]) {
+    score++;
     apple = [
       Math.floor((Math.random() * size) / scale),
       Math.floor((Math.random() * size) / scale)
     ];
   } else {
     snake.pop();
+  }
+
+  // tail collide logic
+  const [h, ...tail] = snake;
+  const didCollideSelf = tail.some(t => t[0] === h[0] && t[1] === h[1]);
+
+  // end
+  if (didCollideSelf || didCollideWall) {
+    const restart = await confirm("Play again");
+    if (restart) {
+      location.reload();
+    } else {
+      clearInterval(intervalId);
+    }
   }
 
   draw();
@@ -61,22 +93,21 @@ function draw() {
   context.fillRect(apple[0] + 0.1, apple[1] + 0.1, 0.8, 0.8);
 }
 
-// todo: increase speed adjustable
+// todo: increase speed automatically
 // like count eaten apples, and create a new interval with faster timeout
-setInterval(update, 300);
+const intervalId = setInterval(update, 300);
 
-// todo: prevent go to opposite direction
 document.body.onkeydown = function(e) {
-  if (e.key === "ArrowDown") {
+  if (e.key === "ArrowDown" && direction !== directions.UP) {
     direction = directions.DOWN;
   }
-  if (e.key === "ArrowUp") {
+  if (e.key === "ArrowUp" && direction !== directions.DOWN) {
     direction = directions.UP;
   }
-  if (e.key === "ArrowLeft") {
+  if (e.key === "ArrowLeft" && direction !== directions.RIGHT) {
     direction = directions.LEFT;
   }
-  if (e.key === "ArrowRight") {
+  if (e.key === "ArrowRight" && direction !== directions.LEFT) {
     direction = directions.RIGHT;
   }
 };
